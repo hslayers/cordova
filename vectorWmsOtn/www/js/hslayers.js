2e5d14d96a03ab3b72121bf16489de7b6cce3496
Šimon Leitgeb <leitgeb.simon@gmail.com>
2015-07-31 17:46:07 +0200

    fixes #5077 by updating the dependencies for cordova project

--- vectorWmsOtn/www/js/hslayers.js
+++ vectorWmsOtn/www/js/hslayers.js
@@ -1,10 +1,10 @@
 'use strict';
 
-/*
-var hsl_path = '../../../../';
+var hsl_path = '';
 require.config({
     paths: {
         toolbar: hsl_path + 'components/toolbar/toolbar',
+        bootstrap: hsl_path + 'bower_components/bootstrap/dist/js/bootstrap.min',
         layermanager: hsl_path + 'components/layermanager/layermanager',
         ows: hsl_path + 'components/ows/ows',
         'ows.wms': hsl_path + 'components/ows/ows_wms',
@@ -15,45 +15,25 @@ require.config({
         print: hsl_path + 'components/print/print',
         permalink: hsl_path + 'components/permalink/permalink',
         lodexplorer: hsl_path + 'components/lodexplorer/lodexplorer',
-        geolocation: hsl_path + 'components/geolocation/geolocation',
+        // geolocation: hsl_path + 'components/geolocation/geolocation',
+        geolocation: 'js/geolocation_cordova',
         measure: hsl_path + 'components/measure/measure',
         legend: hsl_path + 'components/legend/legend',
         app: 'js/app',
         panoramio: hsl_path + 'components/panoramio/panoramio',
         core: hsl_path + 'components/core/core',
-        WfsSource: hsl_path + 'extensions/hs.source.Wfs',
-        api: hsl_path + 'components/api/api',
-        translations: hsl_path + 'components/translations/js/translations'
-    }
-});
-*/
-
-var hsl_path = '';
-require.config({
-    paths: {
-        toolbar: 'components/toolbar/toolbar',
-        layermanager: 'components/layermanager/layermanager',
-        ows: 'components/ows/ows',
-        'ows.wms': 'components/ows/ows_wms',
-        'ows.nonwms': 'components/ows/ows_nonwms',
-        'ows.wmsprioritized': 'components/ows/ows_wmsprioritized',
-        query: 'components/query/query',
-        search: 'components/search/search',
-        print: 'components/print/print',
-        permalink: 'components/permalink/permalink',
-        lodexplorer: 'components/lodexplorer/lodexplorer',
-        // geolocation: 'components/geolocation/geolocation',
-        geolocation: 'js/geolocation_cordova',
-        measure: 'components/measure/measure',
-        legend: 'components/legend/legend',
-        app: 'js/app',
-        panoramio: 'components/panoramio/panoramio',
-        core: 'components/core/core',
         SparqlJson: hsl_path + 'extensions/hs.source.SparqlJson',
-        api: 'components/api/api',
-        WfsSource: 'extensions/hs.source.Wfs',
-        translations: 'components/translations/js/translations',
+        api: hsl_path + 'components/api/api',
+        translations: hsl_path + 'components/translations/js/translations',
         'feature-crossfilter': hsl_path + 'components/feature_crossfilter/f_crossfilter'
+    },
+    shim: {
+        d3: {
+            exports: 'd3'
+        },
+        dc: {
+            deps: ['d3', 'crossfilter']
+        }
     }
 });
 
